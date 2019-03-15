31b727a70425df17bafc6757d17ef52c387d38ac
CodenameCOTC <a.novaladip@yahoo.com>
2019-03-15 18:21:05 +0700

    add: Todo model

---
+++ models/Todo/index.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    maxlength: 200
+  },
+  description: {
+    type: String,
+    maxlength: 400,
+    default: ""
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now()
+  },
+  isFinish: {
+    type: Boolean,
+    default: false
+  },
+  finishDate: {
+    type: Date,
+    default: Date.now()
+  },
+  deadline: {
+    type: Date,
+    required: true
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
+});
+
+module.exports = Todo = mongoose.model("Todo", todoSchema);
