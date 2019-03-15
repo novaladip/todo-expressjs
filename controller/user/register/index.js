2dbae9772db62f8ebcbcb6f49e403174125a32f3
CodenameCOTC <a.novaladip@yahoo.com>
2019-03-15 16:10:01 +0700

    add: user/register controller

---
+++ controller/user/register/index.js
@@ -0,0 +1,45 @@
+const bcrypt = require("bcryptjs");
+const User = require("../../../models/User");
+const { isEmpty, validateRegisterInput } = require("../../../validator");
+
+const register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const { error, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json({ error });
+    }
+
+    const generatedSalt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(password, generatedSalt);
+
+    const userData = {
+      username,
+      password: hashPassword
+    };
+
+    const existingUser = await User.findOne()
+      .where({ username })
+      .exec();
+
+    if (!isEmpty(existingUser)) {
+      return res.status(400).json({
+        error: {
+          username: "Username is already taken"
+        }
+      });
+    }
+
+    const newUser = await User.create(userData);
+    return res.json(newUser);
+  } catch (err) {
+    return res.status(417).json({
+      error: {
+        message: "Unknown error occurs, please try again later."
+      }
+    });
+  }
+};
+
+module.exports = register;
