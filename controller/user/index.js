68514973fbeb4a2d00c95602b0adda0187905edd
CodenameCOTC <a.novaladip@yahoo.com>
2019-03-15 19:42:41 +0700

    enable cors

--- controller/user/index.js
+++ controller/user/index.js
@@ -6,7 +6,6 @@ const userController = {
   register,
   login,
   user: (req, res) => {
-    console.log(req.user);
     User.find()
       .then(users => res.json(users))
       .catch(err => res.json(err));
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const passport = require("passport");
+const cors = require("cors");
 
 const key = require("./config");
 const userRoutes = require("./routes/user");
@@ -12,10 +13,11 @@ const todoRouters = require("./routes/todo");
 const app = server();
 const PORT = process.env.PORT || 5000;
 
+app.use(helmet());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
-app.use(helmet());
 app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
