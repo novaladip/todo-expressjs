abda12e159c39cbbf08d416381638545079c7860
A. Noval Adip <a.novaladip@yahoo.com>
2019-04-26 12:22:24 +0700

    added docker
    Signed-off-by: A. Noval Adip <a.novaladip@yahoo.com>

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 const key = {
   secretOrKey: process.env.secretOrKey,
-  dbURI: process.env.dbURI
+  dbURI: process.env.dbURI,
+  dockerDbURI: process.env.dockerDbURI
 };
 
 module.exports = key;
--- server.js
+++ server.js
@@ -12,25 +12,36 @@ const userRoutes = require("./routes/user");
 const todoRouters = require("./routes/todo");
 
 const app = server();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(cors());
+app.use(morgan());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
 
+app.use("/users", userRoutes);
+app.use("/todo", todoRouters);
+
 mongoose
-  .connect(key.dbURI, {
+  .connect(key.dockerDbURI, {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000
   })
   .then(() => console.log("connected to db"))
   .catch(() => console.log("failed to connect to db"));
 
-app.use("/users", userRoutes);
-app.use("/todo", todoRouters);
+const res404 = {
+  error: {
+    message: "Error 404 routes not found"
+  }
+};
+
+app.all("*", (req, res) => res.status(404).json(res404));
 
 app.listen(PORT, () => console.log(`Server running on localhost:${PORT}`));
